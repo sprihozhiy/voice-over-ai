eca64d94e7af4a01664661d90cbe307f8203611f
Serhii Prykhozhyi <sprihozhiy@gmail.com>
2023-03-03 15:14:37 -0700

    audio to text transcription

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Configuration, OpenAIApi } = require("openai");
+const fs = require('fs');
+const dotenv = require('dotenv')
+dotenv.config({ path: '.env.local' });
+dotenv.config();
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Path to the MP4 video file
+const audioFilePath = 'sassefabian1.mp3';
+
+const openai = new OpenAIApi(configuration);
+
+async function getTranscription() {
+    const resp = await openai.createTranscription(
+        fs.createReadStream(audioFilePath),
+        "whisper-1"
+    );
+
+    // Get the transcript from the response
+    const transcript = resp.data.text;
+
+    // Save the transcript to a text file
+    const transcriptFilePath = 'transcript.txt';
+    fs.writeFileSync(transcriptFilePath, transcript);
+
+    console.log(`Transcription saved to ${transcriptFilePath}`);
+}
+
+getTranscription();
